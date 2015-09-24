24915c3fb2f5c07262c67cba7c5708846086046f
androidgrl <kawaharajamie@gmail.com>
2015-09-24 17:41:44 -0600

    Add simplecov

--- Gemfile
+++ Gemfile
@@ -15,5 +15,6 @@ group :development, :test do
   gem 'web-console', '~> 2.0'
   gem 'rspec-rails', '~> 3.0'
   gem 'spring'
+  gem 'simplecov'
 end
 
---
+++ spec/idea_index_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "Article Comments", :type => :feature, :js => true do
+  before(:each) do
+    visit new_article_path
+    fill_in "article_title", with: "My Article"
+    fill_in "article_body", with: "My article body"
+    click_link_or_button "Save"
+    click_link_or_button "My Article"
+  end
+
+  it "posts a comment" do
+    fill_in "comment_author_name", :with => "Cowboy"
+    fill_in "comment_body", :with => "Testing is too hard."
+    click_link_or_button "post_comment"
+    within('#comments') do
+      expect(page).to have_content("Cowboy said")
+      expect(page).to have_content("Testing is too hard.")
+    end
+  end
+end
