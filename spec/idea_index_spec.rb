234bb9896782a6bb3e81ae91ced0a9671dbe72da
androidgrl <kawaharajamie@gmail.com>
2015-09-24 19:26:56 -0600

    Add test
    Had to require rspec and define the file path.

--- spec/idea_index_spec.rb
+++ spec/idea_index_spec.rb
@@ -1,15 +1,18 @@
 require 'spec_helper'
 
-describe "Article Comments", :type => :feature, :js => true do
+describe "Idea Index", :type => :feature, :js => true do
   before(:each) do
-    visit new_article_path
-    fill_in "article_title", with: "My Article"
-    fill_in "article_body", with: "My article body"
+    visit root_path
+    fill_in "Title", with: "Make Tea"
+    fill_in "Body", with: "Make echinacea tea"
     click_link_or_button "Save"
-    click_link_or_button "My Article"
   end
 
-  it "posts a comment" do
+  it "lists an idea" do
+    expect(page).to have_content("Make Tea")
+  end
+
+  xit "posts a comment" do
     fill_in "comment_author_name", :with => "Cowboy"
     fill_in "comment_body", :with => "Testing is too hard."
     click_link_or_button "post_comment"
