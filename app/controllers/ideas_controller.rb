c75525bfc26947730bd31cea2634bce444e1bb94
androidgrl <kawaharajamie@gmail.com>
2015-10-03 17:06:13 -0600

    Make edit test pass
    Added update action to controller,
    had to remember to pass parameter to update method.

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -19,6 +19,12 @@ class IdeasController < ApplicationController
     @idea = Idea.find(params[:id])
   end
 
+  def update
+    idea = Idea.find(params[:id])
+    idea.update(idea_params)
+    redirect_to root_path
+  end
+
   private
 
   def idea_params
