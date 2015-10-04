5d55b68368d5e1512af94fd564a2908a4e6439d9
androidgrl <kawaharajamie@gmail.com>
2015-10-04 13:41:48 -0600

    Unrefactored
    I unrefactored all the separate functions into one,
    because it's a good exercise for me and to be honest,
    it's clearer to me.

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,17 +1,33 @@
-function formData(){
-    return {
-        idea: {
-            title: $('#idea_title').val(),
-            body: $('#idea_body').val()
-        }
-    };
-}
+//function formData(){
+    //return {
+        //idea: {
+            //title: $('#idea_title').val(),
+            //body: $('#idea_body').val()
+        //}
+    //};
+//}
 
-function submitIdea(){
-    event.preventDefault();
-    console.log(formData());
-}
+//function submitIdea(){
+    //event.preventDefault();
+    //postData();
+//}
+
+//function postData(){
+    //$.post('/ideas', formData());
+//}
+
+//$('document').ready(function(){
+    //$('#new_idea').submit(submitIdea);
+//});
 
 $('document').ready(function(){
-    $('#new_idea').submit(submitIdea);
+    $('#new_idea').submit(function(event){
+        event.preventDefault();
+        $.post('/ideas', {
+            idea: {
+                title: $('#idea_title').val(),
+                body: $('#idea_body').val()
+            }
+        })
+    });
 });
