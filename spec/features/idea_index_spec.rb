13446cae130b572cf10b02c79946672141db782d
androidgrl <kawaharajamie@gmail.com>
2015-10-04 15:14:37 -0600

    Comment out status
    For some reason when testing with js
    it does not have status validations.

--- spec/features/idea_index_spec.rb
+++ spec/features/idea_index_spec.rb
@@ -13,25 +13,25 @@ describe "Idea Index", :type => :feature, :js => true do
     expect_idea_to_have_title(title: "Make Tea")
   end
 
-  xit "lists ideas in descending chronological order" do
+  it "lists ideas in descending chronological order" do
     visit root_path
     fill_in "Title", with: "Make Tea"
     fill_in "Body", with: "Make echinacea tea"
     click_link_or_button "Save"
-    expect(page).to have_http_status(200)
+    #expect(page).to have_http_status(201)
 
     visit root_path
     fill_in "Title", with: "Make Soup"
     fill_in "Body", with: "Make chicken soup"
     click_link_or_button "Save"
-    expect(page).to have_http_status(200)
+    #expect(page).to have_http_status(201)
 
     within all("li")[1] do
       expect(page).to have_content("Make Soup")
     end
   end
 
-  xit "has a body that is truncated to 100 characters rounded to the nearest word" do
+  it "has a body that is truncated to 100 characters rounded to the nearest word" do
     visit root_path
     fill_in "Title", with: "Make Soup"
     fill_in "Body", with: "#{"aaaa " * 30}"
@@ -68,7 +68,7 @@ describe "Idea Index", :type => :feature, :js => true do
     expect(page).to have_content("Swill")
 
     click_on "Thumbs Up"
-    expect(page).to have_http_status(200)
+    #expect(page).to have_http_status(200)
     expect(page).not_to have_content("Swill")
     expect(page).to have_content("Plausible")
   end
@@ -81,7 +81,7 @@ describe "Idea Index", :type => :feature, :js => true do
     click_on "Thumbs Up"
 
     click_on "Thumbs Down"
-    expect(page).to have_http_status(200)
+    #expect(page).to have_http_status(200)
     expect(page).to have_content("Swill")
   end
 end
