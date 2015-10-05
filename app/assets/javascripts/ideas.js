91a1f93aea7673d7a6748f65bcfbd3985158d249
androidgrl <kawaharajamie@gmail.com>
2015-10-04 21:51:06 -0600

    Add body and quality
    Now body and quality are added with ajax.

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -14,10 +14,17 @@ function submitIdea(){
 function postData(){
     $.post('/ideas', formData(), function(data){
         console.log(data);
-        $('#ideas').prepend("<li>" + data.title + "</li>");
+        $('#ideas').prepend(makeIdea(data));
         $('#title').val('');
         $('#body').val('');
     });
+}
+
+function makeIdea(data){
+    return "<li>" + "Title:  " + data.title + "</li>"
+        + "<li>" + "Body:  " + data.body + "</li>"
+        + "<li>Quality:  Swill</li>"
+        + "</br>"
 }
 
 $('document').ready(function(){
