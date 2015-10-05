0a3dedbe918afd2cf4b23e18d2487608c4b8ad77
androidgrl <kawaharajamie@gmail.com>
2015-10-05 10:51:12 -0600

    Use delegate to bind future event handler
    I the problem was that I needed to be able
    to delete something that was newly injected
    via ajax.  This is accomplished with delegate.
    
    Also, I added the buttons to the dom.

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -20,12 +20,33 @@ function postData(){
 }
 
 function makeIdea(data){
+    console.log(data);
     return "<li>" + "Title:  " + data.title + "</li>"
         + "<li>" + "Body:  " + data.body + "</li>"
         + "<li>Quality:  Swill</li>"
+        + "<button class='delete' id="+ data.id +">Delete</button></br>"
+        + "<button id='edit'>Edit</button></br>"
+        + "<button id='thumbs_up'>Thumbs Up</button></br>"
+        + "<button id='thumbs_down'>Thumbs Down</button></br>"
         + "</br>"
 }
 
+function deleteIdea(){
+    console.log("Delete me");
+    $.ajax({
+        url: '/ideas/' + this.id,
+        type: 'DELETE',
+        success: function(result){
+            console.log('I am deleted thank you');
+            $('ul').html("");
+        }
+    });
+}
+
+//$('.delete').on("click", function(){console.log("Hello");});
+
 $('document').ready(function(){
     $('#submit').on("click", submitIdea);
+    $('#ideas').delegate(".delete", "click", deleteIdea)
+    //$('.delete').on("click", deleteIdea);
 });
