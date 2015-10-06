5186403539fd66ea34d2ddd2add0d75850e4245c
androidgrl <kawaharajamie@gmail.com>
2015-10-05 19:49:11 -0600

    Add string truncation
    This cuts to the nearest word so that it
    will be 100 characters or less.  It doesn't
    round up to the nearest word however, only
    rounds down.

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -21,9 +21,11 @@ function postData(){
 
 function makeIdea(data){
     console.log(data);
+    var text = data.body;
+    var truncated = truncateString(text, 100, ' ', '...');
     return  "<div id=idea-" + data.id + ">"
         + "<li>" + "Title:  " + data.title + "</li>"
-        + "<li>" + "Body:  " + data.body + "</li>"
+        + "<li>" + "Body:  " + truncated + "</li>"
         + "<li>Quality:  Swill</li>"
         + "<button class='delete' id="+ data.id +">Delete</button></br>"
         + "<button id='edit'>Edit</button></br>"
@@ -31,6 +33,19 @@ function makeIdea(data){
         + "<button id='thumbs_down'>Thumbs Down</button></br>"
         + "</br>"
         + "</div>"
+}
+
+function truncateString (string, limit, breakChar, rightPad) {
+    if (string.length <= limit) return string;
+
+    var substr = string.substr(0, limit);
+    if ((breakPoint = substr.lastIndexOf(breakChar)) >= 0) {
+        if (breakPoint < string.length -1) {
+            return string.substr(0, breakPoint) + rightPad;
+        }
+    }
+
+    return string;
 }
 
 function deleteIdea(){
@@ -55,7 +70,6 @@ function loadIdeas(){
             data.forEach(function(idea){
                 $('#ideas').append(makeIdea(idea));
             });
-            //need ideas so I can iterate over them
         }
     });
 }
